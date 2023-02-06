65f04d072ceb5cf166b0c4929f729c9b59af972f
lixinggang <lixinggang@bytedance.com>
2023-02-06 20:14:54 +0800

    添加cswin32,注意目前还是prerelease

--- WindowCapture/WindowCapture.cs
+++ WindowCapture/WindowCapture.cs
@@ -1,44 +1,28 @@
 ﻿using Pipeline;
 using PluginCore;
+using System.ComponentModel;
+using Windows.Win32;
 
 namespace WindowCapture
 {
     public class CaptureResult
     {
     }
-    public class WindowCapture : IPipelineNodeSink<CaptureResult>
+    public class WindowCapture
     {
         public CaptureResult Output => throw new NotImplementedException();
-
-        public State TickResult => throw new NotImplementedException();
-
-        public List<IPlugin> Dependencies => throw new NotImplementedException();
-
-        public PluginDesc Desc => throw new NotImplementedException();
-
-        public void Dispose()
+        public string TargetName = string.Empty;
+        public void Init()
         {
-            throw new NotImplementedException();
         }
-
-        public void Init(List<IPlugin> dependencies)
-        {
-            throw new NotImplementedException();
-        }
-
         public void Start()
         {
-            throw new NotImplementedException();
+            //System.Diagnostics.Process(TargetName);
+            FreeLibrarySafeHandle handle;
+            handle = PInvoke.GetModuleHandle(TargetName);
+
+            PInvoke.GetWindowRect(handle, )
         }
 
-        public void Stop()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Tick(float deltaTime)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
