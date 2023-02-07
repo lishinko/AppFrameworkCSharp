43986a5968b6498488bde43f809b767459a6bf3b
lixinggang <lixinggang@bytedance.com>
2023-02-07 20:10:23 +0800

    gdi print window

--- WindowCapture/WindowCapture.cs
+++ WindowCapture/WindowCapture.cs
@@ -1,6 +1,8 @@
 ﻿using Pipeline;
 using PluginCore;
 using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.InteropServices;
 using Windows.Win32;
 
 namespace WindowCapture
@@ -17,11 +19,45 @@ namespace WindowCapture
         }
         public void Start()
         {
-            //System.Diagnostics.Process(TargetName);
-            FreeLibrarySafeHandle handle;
-            handle = PInvoke.GetModuleHandle(TargetName);
+            if (!int.TryParse(TargetName, out int id))
+            {
+                return;
+            }
+            using var process = System.Diagnostics.Process.GetProcessById(id);
+            if (process == null)
+            {
+                return;
 
-            PInvoke.GetWindowRect(handle, )
+            }
+            var hWnd = process.MainWindowHandle;
+            Windows.Win32.Foundation.HWND wnd = new Windows.Win32.Foundation.HWND(hWnd);
+            var hr = PInvoke.GetWindowRect(wnd, out Windows.Win32.Foundation.RECT rect);
+            if (!hr)
+            {
+                return;
+            }
+            using Bitmap bmp = new(rect.Width, rect.Height);
+            using var g = Graphics.FromImage(bmp);
+            IntPtr hdc = g.GetHdc();
+            try
+            {
+                using SafeHandle sh = new DeleteObjectSafeHandle(hdc);
+                hr = PInvoke.PrintWindow(wnd, sh, Windows.Win32.Storage.Xps.PRINT_WINDOW_FLAGS.PW_CLIENTONLY);
+                if (hr)
+                {
+                    bmp.Save(@"e:\save.bmp", System.Drawing.Imaging.ImageFormat.Png);
+                }
+            }
+            finally
+            {
+                g.ReleaseHdc(hdc);
+            }
+            return;
+            //System.Diagnostics.Process(TargetName);
+            //FreeLibrarySafeHandle handle;
+            //handle = PInvoke.GetModuleHandle(TargetName);
+
+            //PInvoke.GetWindowRect(handle, )
         }
 
     }
